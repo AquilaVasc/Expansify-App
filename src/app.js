da561501ab3bcde6f599f2343edca78dd1b40365
= <=>
2019-05-24 19:59:34 -0300

    Adding React Router and basic components

--- src/app.js
+++ src/app.js
@@ -1,35 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import AppRouter from './routers/AppRouter.';
 
-const ExpenseDeshboardPage = () => (
-    <div>This is from my deshboard component</div>
-);
-
-const AddExpensePage = () => (
-    <div>This is from my Add Expense component</div>
-);
-
-const EditExpensePage = () => (
-    <div>This is from my Edit Expense component</div>
-);
-
-const HelpPage = () => (
-    <div>This is from my Help component</div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path="/" component={ExpenseDeshboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage}/>
-        </div>
-        
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
---
+++ src/components/ExpenseDeshboardPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExpenseDeshboardPage = () => (
+    <div>This is from my deshboard component</div>
+);
+
+export default ExpenseDeshboardPage;
